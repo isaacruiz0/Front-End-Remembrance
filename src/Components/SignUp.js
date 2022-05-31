2571c77b134c3242b7509c9254118d9af79ec664
Isaac Ruiz <isaacruiz@Isaacs-MBP.lan>
2022-05-30 19:30:00 -0700

    set up the proper sign up post request to send the user credentials to the backend with axios

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom"
-
+import { useState } from 'react';
+import axios from 'axios'
 
 function SignUp() {
   let navigate = useNavigate();
+  const [name, setName] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+  
+  const signUpRequest = () =>{
+    axios
+      .post(
+        "http://localhost:6000/user/signup",
+        {
+          name,
+          username,
+          password
+        }
+      )
+      .then((res) =>{
+        if (res.status === 200){
+          sessionStorage.setItem("accessToken", res.data.accessToken)
+          sessionStorage.setItem("username", res.data.username)
+          sessionStorage.setItem("name", res.data.name)
+
+        }
+      })
+  }
+
   return (
     <div className='signUp'>
         <div className='leftSideBar'></div>
@@ -14,13 +40,14 @@ function SignUp() {
               <p>Lets start remembering the little things</p>
               <form className='signup-form'>
                 <label>First Name</label>
-                <input type='text' placeholder="" />
+                <input type='text' placeholder="Enter Name" value={name} onChange = {(e) => {setName(e.target.value)}} />
                 <label>Email</label>
-                <input type='text' placeholder="" />
+                <input type='text' placeholder="Enter Email" value={username} onChange = {(e) => {setUsername(e.target.value)}} />
                 <label>Password</label>
-                <input type='text' placeholder="" />
-                <input type="button" value="Submit" />
+                <input type='text' placeholder="Enter Password" value={password} onChange = {(e) => {setPassword(e.target.value)}} />
+                <input type="button" value="Submit" onCliick={signUpRequest} />
               </form>
+              <div className='error'>{error}</div>
             </div>
         </main>
         <div className='rightSideBar'></div>
