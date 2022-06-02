07f26d70ebdc21a4edbee32105fd4af1cedfafd2
Isaac Ruiz <isaacruiz@Isaacs-MBP.lan>
2022-06-02 01:24:46 -0700

    added a hoverable button on the login button and I finished styling the Create Person component

--- src/Components/createPerson/CreatePerson.jsx
+++ src/Components/createPerson/CreatePerson.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react'
 import { useNavigate } from "react-router-dom"
 import apiUrl from '../../apiURL'
 
+import './createPerson.scss'
+
 function CreatePerson() {
     // I am going to need to do a post request and within that i will have use states that are correlated with the user's input
     // these two usestates will handle the change of user input
@@ -76,34 +78,31 @@ function CreatePerson() {
 
 
   return (
-    <div className='create-person-div'>
-        <div className='leftSideBar'></div>
+    <div className='createPerson'>
         <main>
             <div className='person-box'>
-              <h2 className='box-title'>Who would you like to remember?</h2>
               <form className='person-info-form' onSubmit={handleSubmit}>
-                <label>First Name </label>
-                <input type='text' placeholder="" onChange={firstHandleChange} value={firstName}/>
-                <label>Last Name </label>
-                <input type='text' placeholder="" onChange={lastHandleChange} value={lastName}/>
-                <label>Birthday </label>
-                <input type='text' placeholder="" onChange={birthdayHandle} value={birthDay}/>
-                <label>Gender </label>
-                <input type='text' placeholder="" onChange={genderHandle} value={gender}/>
-                <label>Pronouns </label>
-                <input type='text' placeholder="" onChange={pronounsHandle} value={pronouns}/>
-                <label>Relationship </label>
-                <input type='text' placeholder="" onChange={relationshipHandle} value={relationship}/>
-                <input type="submit" value="Submit" />
-                <div className='return-div'>
-                <button className='return-to-dashboard' onClick={()=>navigate('/dashboard')}> Return to Dashboard</button>
-                </div>
-                
+                <div className="inputField">
+                    <label>First Name </label>
+                    <input type='text' placeholder="" onChange={firstHandleChange} value={firstName}/>
+                    <label>Last Name </label>
+                    <input type='text' placeholder="" onChange={lastHandleChange} value={lastName}/>
+                    <label>Birthday </label>
+                    <input type='text' placeholder="" onChange={birthdayHandle} value={birthDay}/>
+                    <label>Gender </label>
+                    <input type='text' placeholder="" onChange={genderHandle} value={gender}/>
+                    <label>Pronouns </label>
+                    <input type='text' placeholder="" onChange={pronounsHandle} value={pronouns}/>
+                    <label>Relationship </label>
+                    <input type='text' placeholder="" onChange={relationshipHandle} value={relationship}/>
+                    <input type="submit" value="Submit" />
+                    <div className='return-div'>
+                    <button className='return-to-dashboard' onClick={()=>navigate('/dashboard')}> Return to Dashboard</button>
+                    </div>
+                </div> 
               </form>
             </div>
-            
         </main>
-        <div className='rightSideBar'></div>
     </div>
   )
 }
