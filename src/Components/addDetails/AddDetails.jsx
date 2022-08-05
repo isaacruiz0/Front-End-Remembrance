b50b444fe426f6eec2c80cdde4f8993d6c6a5127
Isaac <isaac.ruiz0219@gmail.com>
2022-08-04 19:34:17 -0700

    set up a request needed to dynamically display a person's card in the add details page and set up a request route add details to a person

--- src/Components/addDetails/AddDetails.jsx
+++ src/Components/addDetails/AddDetails.jsx
@@ -1,24 +1,80 @@
 import React from 'react'
 import './addDetails.scss'
 import { useState, useEffect } from 'react'
-import {useLocation} from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 import axios from 'axios'
 
 function AddDetails() {
 
     let location = useLocation();
+    const navigate = useNavigate()
 
-    useEffect(() => {
-        const personId = location.state.id
-        const getPerson = async () => {
-            const response = await axios.get(`http://localhost:5000/people/${personId}`)
-            console.log(response.data)
+    const [displayDetails, setDisplayDetails] = useState([])
+    const [inputField, setInputField] = useState('')
+    const [inputTrait, setInputTrait] = useState('')
+    // This is to display the extra details
+    const [displayExtra, setDisplayExtra] = useState('')
+
+    const log = console.log
+    // This idea was passed down from the person we clicked on 
+    const personId = location.state.id
+
+    // This function retrieves the data from a specific person
+    const getPerson = async () => {
+        log(personId)
+        try{
+            // This returns the person of the id that was passed by the useNavigate hook on the card 
+            const response = await axios.get(`http://localhost:5000/people/${personId}/`)
+            const details = await response.data
+            setDisplayDetails(details)
+            let extraDetailsArray = response.data.extraDetails.map((trait) => {
+                return(<h2><span>{trait.key}</span>{trait.value}</h2>)
+            })
+            setDisplayExtra(extraDetailsArray)
+        } catch(err) {
+            console.log(err)
         }
+    }
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault()             
+        const fieldTrait = {extraDetails: [{key: inputField.toString(), value: inputTrait.toString()}]}
+        const response = await axios.put(`http://localhost:5000/people/${personId}`, fieldTrait)
+        log(response)
+
+        setInputField('')
+        setInputTrait('')
+        getPerson()
+
+    }
+    
+    useEffect(() => {
         getPerson()
     },[])
 
+
+
     return (
         <div className='addDetailsComponent'>
+            <h1>Lets add more traits!</h1>
+            <div className="peopleCards">
+                <div className="details">
+                    <h1 className='nameh1'>{displayDetails.firstName} {displayDetails.lastName}</h1>
+                    <h2><span>Birthday</span> {displayDetails.birthDay}</h2>
+                    <h2><span>Gender</span> {displayDetails.gender}</h2>
+                    <h2><span>Pronouns</span> {displayDetails.pronouns}</h2>
+                    <h2><span>Relationship</span> {displayDetails.relationship}</h2>
+                    {displayExtra}
+                </div>
+                <form onSubmit={(handleSubmit)} action="add trait to person">
+                    <div className="inputText">
+                        <input type="text" className='field'placeholder='Field' value={inputField} onInput={(e)=>{setInputField(e.target.value)}}/>
+                        <input type="text" className='trait' placeholder='Trait' value={inputTrait} onInput={(e)=>{setInputTrait(e.target.value)}}/>
+                    </div>
+                    <input type="submit" />
+                    <button className='return-to-dashboard' onClick={()=>navigate('/dashboard')}> Return to Dashboard</button>
+                </form>
+            </div>
         </div>
     )
 }
--- src/Components/dashboard/Dashboard.jsx
+++ src/Components/dashboard/Dashboard.jsx
@@ -36,10 +36,10 @@ function Dashboard() {
                 <div className='peopleCards'  key={index}>
                     <div className="details">
                         <h1 className='nameh1'>{person.firstName} {person.lastName}</h1>
-                        <h2>Birthday: {person.birthDay}</h2>
-                        <h2>Gender: {person.gender}</h2>
-                        <h2>Pronouns: {person.pronouns}</h2>
-                        <h2>Relationship: {person.relationship}</h2>
+                        <h2><span>Birthday</span> {person.birthDay}</h2>
+                        <h2><span>Gender</span> {person.gender}</h2>
+                        <h2><span>Pronouns </span>{person.pronouns}</h2>
+                        <h2><span>Relationship </span>{person.relationship}</h2>
                     </div>
                     <div className="buttonsContainer">
                         <button onClick={handleDelete} id={person._id}>
@@ -76,7 +76,7 @@ function Dashboard() {
         <svg className='topSvg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#E8B987" fill-opacity="1" d="M0,320L48,320C96,320,192,320,288,277.3C384,235,480,149,576,106.7C672,64,768,64,864,53.3C960,43,1056,21,1152,16C1248,11,1344,21,1392,26.7L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
 
 
-            <button className='greeting' onClick={()=>{navigate("/")}} >Hello {sessionStorage.getItem("name")}!</button>
+        <button className='greeting' onClick={()=>{navigate("/")}} >Hello {sessionStorage.getItem("name")}!</button>
 
         <main>
             <div className='div-button'>
