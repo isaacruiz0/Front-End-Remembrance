f373d0414e1f36092ea884522bf703e3f32a700b
Isaac <isaac.ruiz0219@gmail.com>
2022-11-05 13:45:56 -0700

    edited the styling of the create card so now it looks less awkward

--- src/Components/createPerson/CreatePerson.jsx
+++ src/Components/createPerson/CreatePerson.jsx
@@ -97,46 +97,61 @@ function CreatePerson() {
             <header><h1>Create Card</h1></header>
               <form className='person-info-form' onSubmit={handleSubmit}>
                 <div className="inputField">
-                    <label>First Name </label>
-                    <input type='text' placeholder="" onChange={firstHandleChange} value={firstName}/>
-                    <label>Last Name </label>
-                    <input 
-                        type='text'
-                        placeholder="" 
-                        onChange={lastHandleChange} 
-                        value={lastName}/>
-                    <label>Birthday </label>
-                    <input 
-                        type='text' 
-                        placeholder="" 
-                        onChange={birthdayHandle} 
-                        value={birthDay}/>
+                    <div className="inputLabel">
+                        <label>First Name </label>
+                        <input type='text' placeholder="" onChange={firstHandleChange} value={firstName}/>
+                    </div>
+                    <div className="inputLabel">
+                        <label>Last Name </label>
+                        <input 
+                            type='text'
+                            placeholder="" 
+                            onChange={lastHandleChange} 
+                            value={lastName}/>
+                        </div>
+                    <div className="inputLabel">
+                        <label>Birthday </label>    
+                        <input 
+                            type='text' 
+                            placeholder="" 
+                            onChange={birthdayHandle} 
+                            value={birthDay}/>
+                    </div>
+                    <div className="inputLabel">
                         <label>Gender </label>
-                    <input 
-                        type='text' 
-                        placeholder=""
-                        onChange={genderHandle} 
-                        value={gender}/>
-                    <label>Pronouns </label>
-                    <input 
-                        type='text' 
-                        placeholder="" 
-                        onChange={pronounsHandle} 
-                        value={pronouns}/>
-                    <label>Relationship </label>
-                    <input 
-                        type='text' 
-                        placeholder="" 
-                        onChange={relationshipHandle} 
-                        value={relationship}/>
-                    <input type="submit" value="Submit"/>
-                    <div className='return-div'>
-                    <button className='return-to-dashboard' onClick={()=>navigate('/dashboard')}> Return to Dashboard</button>
+                        <input 
+                            type='text' 
+                            placeholder=""
+                            onChange={genderHandle} 
+                            value={gender}/>
+                    </div>
+                    <div className="inputLabel">
+                        <label>Pronouns </label>
+                        <input 
+                            type='text' 
+                            placeholder="" 
+                            onChange={pronounsHandle} 
+                            value={pronouns}/>
+                    </div>
+                    <div className="inputLabel">
+                        <label>Relationship </label>
+                       <input 
+                            type='text' 
+                            placeholder="" 
+                            onChange={relationshipHandle} 
+                            value={relationship}/>
+                    </div>
+                    <div className="submitReturn">
+                        <input type="submit" value="Submit"/>
+                        <div className='return-div'>
+                            <button className='return-to-dashboard' onClick={()=>navigate('/dashboard')}> Return to Dashboard</button>
+                        </div>
                     </div>
                 </div> 
               </form>
             </div>
         </main>
+        <svg className='bottomSvg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#E8B987" fill-opacity="1" d="M0,320L34.3,282.7C68.6,245,137,171,206,128C274.3,85,343,75,411,69.3C480,64,549,64,617,80C685.7,96,754,128,823,170.7C891.4,213,960,267,1029,261.3C1097.1,256,1166,192,1234,181.3C1302.9,171,1371,213,1406,234.7L1440,256L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path></svg> 
     </div>
   )
 }
